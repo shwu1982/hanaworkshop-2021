df564582348bea7c8294cf326f8590f089d31f4f
Shang <shwu@redhat.com>
2021-08-25 18:35:01 +0800

    Update and rename pkg_fix to pkg_fix.sh
    make it a script

---
+++ pkg_fix.sh
@@ -0,0 +1,9 @@
+#/bin/bash
+sudo subscription-manager remove --all
+sudo subscription-manager unregister
+sudo subscription-manager clean
+sudo yum clean all
+sudo rm -rf /var/cache/yum/*
+sudo rm -rf /var/cache/dnf
+sudo yum remove katello-ca-consumer-labsat.opentlc.com
+sudo subscription-manager register --org=7728826 --activationkey=37fc
