5a07166d3db598ab14e2dfcdfd413ee43e3caf9d
Shang <shwu@redhat.com>
2021-09-09 14:53:31 +0800

    Update pkg_fix.sh

--- pkg_fix.sh
+++ pkg_fix.sh
@@ -5,5 +5,5 @@ sudo subscription-manager clean
 sudo yum clean all
 sudo rm -rf /var/cache/yum/*
 sudo rm -rf /var/cache/dnf
-sudo yum remove katello-ca-consumer-labsat.opentlc.com 
+sudo yum remove katello-ca-consumer-labsat.opentlc.com -y
 sudo subscription-manager register --org=7728826 --activationkey=37fc
